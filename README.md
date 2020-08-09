9e799ca35452d8f57c60430227fffcc36cb447c0
mothy900 <mothy900@naver.com>
2020-08-09 22:15:22 +0900

    react map function

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Movie app 2020
 
-React JS Fundaemantals Course (2020 update)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>I love {name} </h3>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://www.platingsandpairings.com/wp-content/uploads/2014/12/Kimchi-5.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG",
+  },
+  {
+    name: "sushi",
+    imgae: "https://i.ytimg.com/vi/xLV1ZHEAaS0/maxresdefault.jpg",
+  },
+  {
+    name: "chiken",
+    image: "https://i.ytimg.com/vi/zYkA5W_YeTY/maxresdefault.jpg",
+  },
+  {
+    name: "bagle",
+    image:
+      "https://uvmbored.com/wp-content/uploads/2016/04/Purchase_Coffee_and_Bagel.jpg",
+  },
+  {
+    name: "maratang",
+    image:
+      "https://s3-media4.fl.yelpcdn.com/bphoto/ZRznFtvHMlJSoIfMaxYkcw/o.jpg",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
