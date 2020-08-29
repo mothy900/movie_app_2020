4ece837828f0407c2b7db338b2892209ebffadbd
mothy900 <mothy900@naver.com>
2020-08-29 17:21:23 +0900

    Updates

---
+++ precache-manifest.0c33d754ace93f136c0062620bbd28da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b68af99c978a1d15256a85aecfeb819",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "987625a2ff4cf5bb9df4",
+    "url": "/movie_app_2020/static/css/main.2c9039cd.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987625a2ff4cf5bb9df4",
+    "url": "/movie_app_2020/static/js/main.0a4b664b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
